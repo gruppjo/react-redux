eea5a3c3fdacc884fc67b7c39bf13e286f080deb
gruppjo <jona_blubb@web.de>
2017-12-12 23:31:53 +0100

    lesson1: onChange={(event) => {console.log ...}}

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -3,11 +3,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component { // class-based component
   render() {
-    return <input onChange={this.handleInputChange}/>;
-  }
-
-  handleInputChange(event) {
-    console.log(event.target.value);
+    return <input onChange={(event) => { console.log(event.target.value) }}/>;
   }
 }
 
