e7b7fd924cb08dadc59b3077c1c8b0bb7ebe12ad
gruppjo <jona_blubb@web.de>
2017-12-12 23:03:15 +0100

    component stubs and key setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import APIKEY from 'keys';
+
+// Create a new component. This component should produce
+// some HTML
+const App = () => {
+  return <div>hi</div>;
+};
+
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(
+  <App/>, // instanciate App Class
+  document.querySelector('.container')
+);
