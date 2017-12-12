b3d2f1e51aca3701f056991ea08f8d7fd6b273ea
gruppjo <jona_blubb@web.de>
2017-12-12 23:30:02 +0100

    lesson1: handleInputChange

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component { // class-based component
   render() {
-    return <input/>;
+    return <input onChange={this.handleInputChange}/>;
+  }
+
+  handleInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
